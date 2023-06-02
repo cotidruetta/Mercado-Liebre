6b3e476e12c54188e067573ef0e253cb3160150e
cotidruetta <cotidruetta1@gmail.com>
2023-06-02 16:37:47 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require ('express');
+const path = require ('path');
+
+const app = express();
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get ('/home', function (req,res){
+    res.sendFile (path.join (__dirname, './views/index.html'));
+});
+
+app.get ('*', function (req,res){
+    res.send ("error. No hemos encontrado esa direccion")
+});
+
+app.listen (3030, () => console.log('servidor corriendo :)'));
