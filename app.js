f28ad88db3e2f56eca43f54f2500108353e25399
cotidruetta <cotidruetta1@gmail.com>
2023-06-07 03:27:28 -0300

    formularios

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.get ('/home', function (req,res){
     res.sendFile (path.join (__dirname, './views/index.html'));
 });
 
+app.get ('/register', function (req,res){
+    res.sendFile (path.join (__dirname, './views/register.html'))
+})
+
+app.get ('/login', function (req,res){
+    res.sendFile (path.join (__dirname, './views/login.html'))
+})
+
 app.get ('*', function (req,res){
     res.send ("error. No hemos encontrado esa direccion")
 });
