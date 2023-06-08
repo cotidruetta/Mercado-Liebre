70444a0429a3e25e32716d080009c1680aff70e6
cotidruetta <cotidruetta1@gmail.com>
2023-06-08 18:11:17 -0300

    app.js modificado

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require ('path');
 const app = express();
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get ('/home', function (req,res){
+app.get ('/', function (req,res){
     res.sendFile (path.join (__dirname, './views/index.html'));
 });
 
